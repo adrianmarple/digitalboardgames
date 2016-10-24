c5c86f8771f1003f905187c66c43aadd13f6f794
Adrian Marple <adrian.marple@gmail.com>
2016-10-23 23:47:41 -0700

    Main starts with logic in place and it is now theoretically playable.

--- public/startswith/startswith.js
+++ public/startswith/startswith.js
@@ -16,12 +16,42 @@ app.controller('StartsWithController', function($scope, $interval) {
     "Fictional Character",
     "Historical Figure",
     "Idiom",
+    "Book",
+    "Movie",
+    "Game",
+    "City",
+    "Office Supply",
+    "Company",
+    "Body Part",
+    "Animal",
+    "Language",
   ];
   var POSSIBLE_STARTING_LETTERS = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "r", "s", "t", "w",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "P",
+    "R",
+    "S",
+    "T",
+    "W",
+    "X or O",
+    "Q or Z",
+    "U or V",
   ];
   var CATEGORIES_PER_ROUND = 10;
-  var ROUND_DURATION = 1 * 10 * 1000;
+  var ROUND_DURATION = 1 * 60 * 1000;
 
   var gameRef = firebase.database().ref("games/idgoeshere/game");
 
@@ -49,12 +79,6 @@ app.controller('StartsWithController', function($scope, $interval) {
     update();
   };
 
-  // $scope.latestRound = function() {
-  //   if (!$scope.game || $scope.game.rounds.length == 0) {
-  //     return {};
-  //   }
-  //   return $scope.game.rounds[$scope.game.rounds.length - 1];
-  // };
 
   $scope.timerString = "";
   var setTimerString = function() {
@@ -73,17 +97,52 @@ app.controller('StartsWithController', function($scope, $interval) {
   $interval(setTimerString, 100);
 
   $scope.setScore = function() {
-    if (!$scope.game.rounds[0][$scope.uid]) {
-      $scope.game.rounds[0][$scope.uid] = {};
-    }
-    $scope.game.rounds[0][$scope.uid].score = $scope.calculateScore();
     $scope.game.isBetweenRounds = true;
     update();
   }
 
-  $scope.calculateScore = function() {
-    return 0;
-  };
+  $scope.calculateRoundScore = function() {
+    try {
+      var categories = $scope.game.rounds[0].categories
+    } catch(err) {
+      return 0;
+    }
+    var score = 0;
+    for (var i = 0; i < categories.length; i++) {
+      try {
+        var entry = $scope.game.rounds[0][$scope.uid][categories[i]].entry;
+      } catch(err) { continue; }
+      if ($scope.game.rounds[0][$scope.uid][categories[i]].isInvalid) {
+        continue;
+      }
 
+      var startingLetter = $scope.game.rounds[0].startingLetter;
+      score += entry.toUpperCase().split(" ").filter(function(word) {
+        return word[0] == startingLetter[0] ||
+            word[0] == startingLetter[startingLetter.length - 1];
+      }).length;
+    }
+
+    if (!$scope.game.rounds[0][$scope.uid]) {
+      $scope.game.rounds[0][$scope.uid] = {};
+    }
+    $scope.game.rounds[0][$scope.uid].score = score;
+    
+    return score;
+  };
+  $scope.calculateTotalScore = function() {
+    if (!$scope.game || !$scope.game.rounds) {
+      return 0;
+    }
+    return $scope.game.rounds.reduce(function(value, round) {
+      try {
+        return value + round[$scope.uid].score;
+      } catch(err) {
+        return value;
+      }
+    }, 0);
+  }
+
+  $scope.update = update;
   function update() { updateFirebase(gameRef, $scope.game); }
 });
